ced81b26466cdff262f9558d54555803da3142b8
Kornel_Olechno <olechnokornel@gmail.com>
2021-04-27 00:10:52 +0200

    Dodaj pliki projektów.

---
+++ FizzBuzz3/Models/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz3.Models
+{
+    public class FizzBuzz
+    {
+        public int Number { get; set; }
+        public string result { get; set; }
+
+        public void FizzBuzzWyswietl()
+        {
+            if(Number >= 1 && Number <= 1000)
+            {
+                if(Number%3==0)
+                {
+                    result += "Fizz";
+                }
+                if(Number%5==0)
+                {
+                    result += "Buzz";
+                }
+                else
+                {
+                    result = "Podana liczba nie spelnia kryteriow FizzBuzz";
+                }
+            }
+        }
+    }
+}
