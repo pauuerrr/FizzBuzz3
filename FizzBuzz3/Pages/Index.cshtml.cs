1b57ff9c6291fd63164c7c95dc2dd736aa0b8889
Kornel_Olechno <olechnokornel@gmail.com>
2021-04-27 00:37:36 +0200

    Zmieniono kod formularza na formularz z RazorTagHelpers

--- FizzBuzz3/Pages/Index.cshtml.cs
+++ FizzBuzz3/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FizzBuzz3.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,6 +12,7 @@ namespace FizzBuzz3.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public FizzBuzz FizzBuzz { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
