5ce56d38277a459653929429b7db26c261b0a7e4
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-13 16:32:30 +0200

    Dodanie parametrów w AddSession()

--- FizzBuzz3/Startup.cs
+++ FizzBuzz3/Startup.cs
@@ -24,8 +24,13 @@ namespace FizzBuzz3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddMemoryCache();
-            services.AddSession();
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromSeconds(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
