17dd568d6d814a376d2a00056bab3f51ddd1472e
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-13 16:37:47 +0200

    Dodanie elementu nawigacji do pliku _Layout.cshtml

---
+++ FizzBuzz3/Pages/Ostatnio_szukane.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzz3.Pages
+{
+    public class Ostatnio_szukaneModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
