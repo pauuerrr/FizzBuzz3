d1d4362defa4322000d8bacca566f65f0f23ea9a
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-18 01:51:31 +0200

    Utworzenie katalogu Data z klasą kontekstu

---
+++ FizzBuzz3/Data/FizzBuzzContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FizzBuzz3.Models;
+
+namespace FizzBuzz3.Data
+{
+    public class FizzBuzzContext : DbContext
+    {
+        public DbSet<FizzBuzz> FizzBuzz { get; set; }
+    }
+}
