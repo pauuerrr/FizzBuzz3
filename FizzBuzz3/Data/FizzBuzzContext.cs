257dc3dbc0de4d4b2785323c1507f575f3ca7f06
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-18 02:04:11 +0200

    Dodanie konstruktora do klasy FizzBuzzContext,skofigurowanie polaczenia z bazą w Startup.cs

--- FizzBuzz3/Data/FizzBuzzContext.cs
+++ FizzBuzz3/Data/FizzBuzzContext.cs
@@ -9,6 +9,7 @@ namespace FizzBuzz3.Data
 {
     public class FizzBuzzContext : DbContext
     {
+        public FizzBuzzContext(DbContextOptions options) : base(options) { }
         public DbSet<FizzBuzz> FizzBuzz { get; set; }
     }
 }
