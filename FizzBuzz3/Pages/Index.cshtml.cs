50be979eaa29e7bc9f6f9cfd9d13e2a4d8795106
Kornel_Olechno <olechnokornel@gmail.com>
2021-05-18 03:03:38 +0200

    Modyfiacja pliku index.cs.html.cs

--- FizzBuzz3/Pages/Index.cshtml.cs
+++ FizzBuzz3/Pages/Index.cshtml.cs
@@ -8,26 +8,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
-
-
-
+using FizzBuzz3.Data;
 
 namespace FizzBuzz3.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly FizzBuzzContext _context;
+        public IList<FizzBuzz> Fizzbuzzes { get; set; }
         [BindProperty]
         public FizzBuzz FizzBz { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, FizzBuzzContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
-            
+            Fizzbuzzes = _context.FizzBuzz.ToList(); 
         }
 
         public IActionResult OnPost()
